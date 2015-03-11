1df9c9ae9dad4f7662c515885ae041814088c6d1
Kidy <kidylee@gmail.com>
2015-03-11 19:33:34 -0400

    init

---
+++ Happiness/HappinessViewController.swift
@@ -0,0 +1,15 @@
+//
+//  HappinessViewController.swift
+//  Happiness
+//
+//  Created by Li An on 3/11/15.
+//  Copyright (c) 2015 LA. All rights reserved.
+//
+
+import UIKit
+
+class HappinessViewController: UIViewController
+{
+
+    
+}
