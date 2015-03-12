818ddc37456d8d4ae55cad289f87fe9960e1d59b
Kidy <kidylee@gmail.com>
2015-03-12 00:04:21 -0400

    Lecture6

--- Happiness/FaceView.swift
+++ Happiness/FaceView.swift
@@ -8,25 +8,34 @@
 
 import UIKit
 
+
+protocol FaceViewDataSource: class{
+    func smilinessForFaceView(sender: FaceView) -> Double?
+}
+
+@IBDesignable
 class FaceView: UIView
 {
+    @IBInspectable
     var lineWidth: CGFloat = 3 {
         didSet {
             setNeedsDisplay()
         }
     }
-    
+    @IBInspectable
     var scale: CGFloat = 0.90 { didSet{ setNeedsDisplay()  }}
-    
+    @IBInspectable
     var color: UIColor = UIColor.blueColor(){ didSet { setNeedsDisplay() } }
     
     var faceCenter: CGPoint{
         return convertPoint(center, fromView: superview)
     }
-
+    
     var faceRadius: CGFloat{
         return min(bounds.size.width, bounds.size.height) / 2 * scale
     }
+    
+    weak var dataSource: FaceViewDataSource?
     
     private struct Scaling{
         static let FaceRadiusToEyeRadiusRatio: CGFloat = 10
@@ -89,13 +98,18 @@ class FaceView: UIView
         
         bezierPathForEye(.Left).stroke()
         bezierPathForEye(.Right).stroke()
-
-        let smiliness = -0.75
+        
+        let smiliness = dataSource?.smilinessForFaceView(self) ?? 0.0
         let smilePath = bezierPathForSmile(smiliness)
         smilePath.stroke()
     }
     
+    func scale(gesture: UIPinchGestureRecognizer){
+        if gesture.state == .Changed{
+            scale *= gesture.scale
+            gesture.scale = 1
+        }
+    }
     
     
-
 }
--- Happiness/HappinessViewController.swift
+++ Happiness/HappinessViewController.swift
@@ -8,8 +8,53 @@
 
 import UIKit
 
-class HappinessViewController: UIViewController
+class HappinessViewController: UIViewController, FaceViewDataSource
 {
-
     
+    @IBOutlet weak var faceView: FaceView! {
+        didSet{
+            faceView.dataSource = self
+            faceView.addGestureRecognizer(UIPinchGestureRecognizer(target: faceView, action: "scale:"))
+        }
+    }
+    
+    private struct Constants{
+        static let HappinessGestureScals: CGFloat = 4
+    }
+    
+    @IBAction func changeHappiness(gesture: UIPanGestureRecognizer) {
+        switch gesture.state{
+        case .Ended: fallthrough
+        case .Changed:
+            let translation = gesture.translationInView(faceView)
+            let happinessChange = -Int(translation.y / Constants.HappinessGestureScals)
+            if happinessChange != 0 {
+                happiness += happinessChange
+                gesture.setTranslation(CGPointZero, inView: faceView)
+                
+                
+            }
+        default: break
+        }
+        
+        
+    }
+    
+    
+    var happiness: Int = 50 { // 0 = very sad, 100 = ecstatic
+        didSet{
+            happiness = min(max(happiness, 0),100)
+            println("happiness = \(happiness)")
+            updateUI()
+        }
+    }
+    
+    func updateUI(){
+        faceView.setNeedsDisplay()
+    }
+    
+    
+    func smilinessForFaceView(sender: FaceView) -> Double? {
+        return Double(happiness - 50) / 50
+    }
 }
